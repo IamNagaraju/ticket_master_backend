4d6aacf5c4b41ee79e49f80ab862807173abfba9
Nagaraju <pallipatinagaraju@gmail.com>
2018-08-10 10:07:43 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const bodyParser = require('body-parser') 
+
+const morgan =require('morgan')
+
+const app = express();
+
+const port = 3000;
+// MiddleWare
+app.use(bodyParser.json());
+
+app.use(morgan('short'));
+
+app.get('/',(req,res)=>{
+    res.send({msg:'welcome to ticket master'})
+})
+
+app.listen(port,()=>{
+    console.log('listing to port num '+port);
+})
